747ffb61e92f387e621b432318046a577388598a
codemodeactivate <jeff.zenko@gmail.com>
2023-05-15 22:58:03 -0400

    Category and Project done

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,48 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, // integer
+      allowNull: false, // doesn't allow null values
+      primaryKey: true, // set as primary key
+      autoIncrement: true, // uses auto increment
   },
+  product_name: {
+      type: DataTypes.STRING, // string
+      allowNull: false, // doesn't allow null values
+  },
+  price: {
+      type: DataTypes.DECIMAL, // decimal
+      allowNull: false, // doesn't allow null values
+      // validates that the value is numeric
+      validate: {
+          isDecimal: {
+            msg: 'Must be a decimal value',
+          }
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER, // integer
+      allowNull: false, // doesn't allow null values
+      // set default value to 10
+      defaultValue: 10,
+      // validates that the value is numeric
+      validate: {
+          isNumeric: {
+            msg: 'Must be a numeric value',
+          }
+        },
+      },
+      category_id: {
+        type: DataTypes.INTEGER, // integer
+        // references the category model's id
+        references: {
+          model: 'category',
+          key: 'id',
+        },
+      },
+
+},
   {
     sequelize,
     timestamps: false,
