9be0300387c84562690e25a90c7e6481d44eaade
codemodeactivate <jeff.zenko@gmail.com>
2023-05-18 08:31:41 -0400

    Tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -7,7 +7,16 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, // integer
+      allowNull: false, // doesn't allow null values
+      primaryKey: true, // set as primary key
+      autoIncrement: true, // uses auto increment
   },
+  tag_name: {
+      type: DataTypes.STRING, // string
+  },
+},
   {
     sequelize,
     timestamps: false,
