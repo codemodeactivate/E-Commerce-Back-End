423229cf2a0023b5dc84a0b64bc7ff164287f484
codemodeactivate <jeff.zenko@gmail.com>
2023-05-19 19:37:09 -0400

    get category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,9 +18,20 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  // Find one category by its `id` value and include its associated Products
+  Category.findOne({
+    where: { id: req.params.id },
+    include: Product
+  })
+    .then((categoryData) => {
+      res.status(200).json(categoryData);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: 'Internal server error' });
+    });
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
