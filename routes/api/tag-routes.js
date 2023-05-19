1f89085a73ea217375e867d9b55a23a75215c442
codemodeactivate <jeff.zenko@gmail.com>
2023-05-19 19:01:00 -0400

    get all tags & get tag /:id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,31 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{ model: Product }],
+  })
+    .then((tagData) => {
+      if (!tagData) {
+        res.status(404).json({ message: 'No tag found with this id!' });
+        return;
+      }
+      res.status(200).json(tagData);
+    })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [{ model: Product }],
+  })
+    .then((tagData) => {
+      if (!tagData) {
+        res.status(404).json({ message: 'No tag found with this id!' });
+        return;
+      }
+      res.status(200).json(tagData);
+    });
 });
 
 router.post('/', (req, res) => {
